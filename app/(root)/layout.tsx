d99d04818e25d8d07248a99c860e7c376bd248b4
Zümra <zumrakb8@gmail.com>
2024-11-16 00:58:13 +0300

    cookies

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,8 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import React from "react";
 import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
+
+export const dynamic = "force-dynamic";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
